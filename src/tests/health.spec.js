9804a02e1df54df944c05afac4b2376059692a0b
João Pedro <jjoao.monteiro15@gmail.com>
2022-08-21 18:20:34 -0300

    test: create a first block of tests in route get health sync and async

--- src/tests/health.spec.js
+++ src/tests/health.spec.js
@@ -1,1 +1,19 @@
-console.log('teste')
+const { expect } = require('chai')
+const { describe, it } = require('mocha')
+const { healthCheckAsync, healthCheckSync } = require('../controllers/health.controller')
+
+describe('Test /health', () => {
+   describe('Health check on /sync', () => {
+    it('health should be okey', () => {
+        const actualResult = healthCheckSync()
+        expect(actualResult).to.equal('OK')
+    })
+   })
+
+   describe('Health check on /async', () => {
+    it('health should be okay', async () => {
+        const actualResult = await healthCheckSync()
+        expect(actualResult).to.equal('OK')
+    })
+   })
+})
