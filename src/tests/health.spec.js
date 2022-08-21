631aa924859f4e19cc4aa8d44cb4114e7264ac0b
João Pedro <jjoao.monteiro15@gmail.com>
2022-08-21 18:52:35 -0300

    test: create a hooks in suit tests. Using before, beforeEach, after and afterEach.

--- src/tests/health.spec.js
+++ src/tests/health.spec.js
@@ -1,8 +1,26 @@
 const { expect } = require('chai')
-const { describe, it } = require('mocha')
+const { describe, it, before, beforeEach, after, afterEach } = require('mocha')
 const { healthCheckAsync, healthCheckSync } = require('../controllers/health.controller')
 
 describe('Test /health', () => {
+
+    before('before', () => {
+        console.log('this log referring a before all tests in describe Test /health')
+    })
+
+    after('after', () => {
+        console.log('this log referring a after all tests in describe Test /health')
+    })
+
+    beforeEach('beforeEach', () => {
+        console.log('this log in running before on all blocks it in describe');
+    })
+
+    afterEach('afterEach', () => {
+        console.log('this log in running after on all blocks it in describe');
+    })
+
+
    describe('Health check on /sync', () => {
     it('health should be okey', () => {
         const actualResult = healthCheckSync()
