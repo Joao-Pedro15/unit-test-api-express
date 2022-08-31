f5995f15ec4a9c681a5cfe951ecab36158db3764
João Pedro <jjoao.monteiro15@gmail.com>
2022-08-30 21:36:41 -0300

    feat: create a route /:hash from item.route

--- src/routes/item.route.js
+++ src/routes/item.route.js
@@ -2,6 +2,24 @@ const express = require('express')
 const itemController = require('../controllers/item.controller')
 const router = express.Router()
 
+router.get('/:hash', async (req, res) => {
+    try{
+        const { hash } = req.params
+        const item = await itemController.readItem(hash)
+        res.json({
+            item,
+            status: 200,
+            message: 'Item read successfully!'
+        })
+    }catch(err){
+        res.json({
+            item: null,
+            status: err.code || err.statusCode || 500,
+            message: err.message || 'Something went wrong while reading item from DB!'
+        })
+    }
+})
+
 router.get('/test', async (req, res) => {
     const result = await itemController.readItem('1233435893')
     return res.json(result)
